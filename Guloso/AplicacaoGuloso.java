ae0c1391049bbcea44827631ed00281333659c57
Leonardo Augusto Pereira do Carmo <78862366+Leozin11@users.noreply.github.com>
2023-05-28 22:07:00 -0300

    Update AplicacaoGuloso.java

--- Guloso/AplicacaoGuloso.java
+++ Guloso/AplicacaoGuloso.java
@@ -4,44 +4,57 @@ import java.util.List;
 import java.util.Random;
 
 public class TesteAutomatizado {
+
     static Random aleatorio = new Random();
-    static final long TEMPO_LIMITE = 4 * 60 * 1000; // 4 minutos em milissegundos
 
     public static void main(String[] args) {
-        int tamanhoInicial = 5;
-        int tamanhoMaximo = 100;
-        int repeticoes = 70;
+        int iteracoes = 70;
+        int limiteVertices = 100;
+        int incrementoVertices = 5;
 
-        int tamanhoConjunto = tamanhoInicial;
+        System.out.println("Teste do Caixeiro Viajante");
 
-        while (tamanhoConjunto <= tamanhoMaximo) {
-            List<Long> tempos = new ArrayList<>();
+        for (int vertices = 5; vertices <= limiteVertices; vertices += incrementoVertices) {
+            System.out.println("Número de Vértices: " + vertices);
+            long tempoTotal = 0;
+            int iteracao;
 
-            for (int i = 0; i < repeticoes; i++) {
-                int[][] grafo = grafoCompletoPonderado(tamanhoConjunto);
+            for (iteracao = 1; iteracao <= iteracoes; iteracao++) {
+                int[][] grafo = grafoCompletoPonderado(vertices);
+
                 long tempoInicial = System.currentTimeMillis();
-                caixeiroViajanteGuloso(grafo);
-                long tempoFinal = System.currentTimeMillis();
-                long tempoExecucao = tempoFinal - tempoInicial;
-                tempos.add(tempoExecucao);
+                long tempoLimite = tempoInicial + 4 * 60 * 1000; // 4 minutos
+                List<Integer> caminho = null;
 
-                if (tempoExecucao > TEMPO_LIMITE) {
+                while (caminho == null && System.currentTimeMillis() < tempoLimite) {
+                    caminho = caixeiroViajanteGuloso(grafo);
+                }
+
+                long tempoFinal = System.currentTimeMillis();
+
+                if (caminho != null) {
+                    long tempoIteracao = tempoFinal - tempoInicial;
+                    tempoTotal += tempoIteracao;
+
+                    System.out.println("Iteração " + iteracao + ": Tempo de Solução: " + tempoIteracao + " ms");
+
+                    if (tempoIteracao >= 4 * 60 * 1000) {
+                        break;
+                    }
+                } else {
+                    System.out.println("Iteração " + iteracao + ": Tempo Limite Excedido");
                     break;
                 }
             }
 
-            long tempoMedio = calcularTempoMedio(tempos);
-
-            System.out.println("Tamanho do conjunto: " + tamanhoConjunto);
-            System.out.println("Tempo médio de solução: " + tempoMedio + " ms");
-
-            if (tempoMedio > TEMPO_LIMITE) {
-                int verticesLimite = tamanhoConjunto - 1;
-                System.out.println("Número N-1: " + verticesLimite);
-                break;
+            if (iteracao > iteracoes) {
+                System.out.println("Tempo Limite Excedido");
+            } else {
+                long tempoMedio = tempoTotal / iteracao;
+                System.out.println("Tempo Médio: " + tempoMedio + " ms");
             }
 
-            tamanhoConjunto++;
+            System.out.println("------------------------------------");
         }
     }
 
@@ -59,7 +72,7 @@ public class TesteAutomatizado {
         return matriz;
     }
 
-    public static void caixeiroViajanteGuloso(int[][] grafo) {
+    public static List<Integer> caixeiroViajanteGuloso(int[][] grafo) {
         List<Integer> caminho = new ArrayList<>();
         int vertices = grafo.length;
 
@@ -75,8 +88,7 @@ public class TesteAutomatizado {
 
         caminho.add(0); // Voltar para a cidade de origem
 
-        System.out.println("Caminho percorrido: " + caminho);
-        System.out.println("Distância total: " + calcularDistanciaCaminho(grafo, caminho));
+        return caminho;
     }
 
     public static int encontrarProximoVertice(int[][] grafo, int verticeAtual, boolean[] visitados) {
@@ -91,23 +103,5 @@ public class TesteAutomatizado {
         }
 
         return proximoVertice;
-    }
-
-    public static int calcularDistanciaCaminho(int[][] grafo, List<Integer> caminho) {
-        int distancia = 0;
-        for (int i = 0; i < caminho.size() - 1; i++) {
-            int origem = caminho.get(i);
-            int destino = caminho.get(i + 1);
-            distancia += grafo[origem][destino];
-        }
-        return distancia;
-    }
-
-    public static long calcularTempoMedio(List<Long> tempos) {
-        long soma = 0;
-        for (Long tempo : tempos) {
-            soma += tempo;
-        }
-        return tempos.isEmpty() ? 0 : soma / tempos.size();
     }
 }
